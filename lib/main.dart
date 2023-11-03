f8dd7d068501eeee1a425cedbf99de5b657b043c
Abdul Rafay Bin Yousuf <rafaybinyousuf314@gmail.com>
2023-11-03 10:54:44 +0500

    an app which will give random name for people who are lazy
    app will give you random with awfully beautiful UI

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  
+  
+void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+}
+ var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;  
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            BigCard(),
+            Text(pair.asLowerCase),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              
+              ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                 
+                  child: Text('Next Kr'),
+                ),
+            ],
+          ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
+  
+  
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); 
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text('A random baap word:', 
+        style: style,),
+      ),
+    );
+  }
+}
